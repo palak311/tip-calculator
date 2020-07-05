3bef5b97c4e758b8c51334df5adea96f9c52095d
Palak <61751687+palak311@users.noreply.github.com>
2020-07-05 16:19:23 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,36 @@
+let amount = document.querySelector('#bill');
+let percent = document.querySelector('#tip');
+let people = document.querySelector('#no_of_people');
+let calculate = document.querySelector("#Calculate_button");
+let des = document.querySelector('#each');
+let tip = document.querySelector('#tip_indivisual');
+
+//calculate button
+calculate.addEventListener('click', () => {
+    //inavlid input
+    alert("gsdc");
+    if(amount == '' || people == ''){
+        alert("please enter the required imformation.thank you.");
+        return;
+    }
+
+    if(people == ''|| people <= 1 ){
+        people = 1;
+        des.innerHTML = '';
+    }
+    
+    //calculate the total tip
+    let total = (amount.value * percent.value) / people.value ;
+    total = Math.round(total*100) / 100 ; // rounding values
+    total = total.toFixed(2);
+    if(total ==='NaN'){
+        total = "0.00";
+    }
+    tip.innerHTML = total; 
+
+    //resetting the input boxes
+    amount.value = '';
+    people.value = '';
+    percent.value = '0';
+})
+
